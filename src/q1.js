6886988f182a50671b1d54c3347b24f911d45cd3
Max Ma <marfi241@gmail.com>
2017-03-16 18:08:51 +0800

    q1 add recursive method

--- src/q1.js
+++ src/q1.js
@@ -8,3 +8,13 @@ const reverseString = (s) => {
 }
 
 console.log(reverseString("hello"));
+
+const reverseStringRecursive = (str) => {
+    if (str === "") {
+        return "";
+    } else {
+        return reverseStringRecursive(str.substr(1)) + str.charAt(0);
+    }
+}
+
+console.log(reverseStringRecursive("abc"));
